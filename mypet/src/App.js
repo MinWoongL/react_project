62bd5466955a327e3ee1a40be99ebf6c16384bac
MinWoongL <dlalsdnd2@gmail.com>
2023-07-25 09:08:18 +0900

    mypet

--- mypet/src/App.js
+++ mypet/src/App.js
@@ -1,61 +1,77 @@
-// App.js
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import { observer } from 'mobx-react'
-import themeStore from './stores/ThemeStore'
-import { AppBar, Button, Container, Toolbar, Typography, useMediaQuery } from '@mui/material'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import Home from './views/Home'
-import About from './views/About'
-import SignInSide from './views/SignInSide'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './stores/index';
+import { AppBar, Button, Toolbar, Typography, Grid, Box } from '@mui/material'
+import MainPage from './pages/MainPage';
+import ShelterPage from './pages/Shelter';
+import AdoptionPage from './pages/Adoption';
+import BoardPage from './pages/Board';
+import MyPage from './pages/MyPage';
+import InfoSidebar from './components/Sidebar/InfoSidebar'
+import RankSystemSidebar from './components/Sidebar/RankSystemSidebar'
+import './styles/base.css'
 
-function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  
-  const theme = React.useMemo(
-    function() {
-      return createTheme({
-        palette: {
-          mode: themeStore.theme === 'dark' || prefersDarkMode ? 'dark' : 'light',
-        },
-      });
-    },
-    [prefersDarkMode, themeStore.theme]
-  )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
 
+function NavBar() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              HappyDog!
-            </Typography>
-            <Button color="inherit" onClick={function() { themeStore.toggleTheme(); }}>
-              Toggle Theme
-            </Button>
-            <Button color="inherit" component={Link} to="/">Home</Button>
-            <Button color="inherit" component={Link} to="/about">About</Button>
-            <Button color="inherit" component={Link} to="/signin">Sign In</Button>
-          </Toolbar>
-        </AppBar>
-        <Routes>
-          <Route path="/about" element={
-            <Container>
-              <About />
-            </Container>
-          }/>
-          <Route path="/signin" element={<SignInSide />} />
-          <Route path="/" element={
-            <Container>
-              <Home />
-            </Container>
-          } exact />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <AppBar position="static" className='theme-blueberry' style={{ backgroundColor : 'var(--dark)'}}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          HappyDog!
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/shelter">보호소</Button>
+        <Button color="inherit" component={Link} to="/adoption">입양하기</Button>
+        <Button color="inherit" component={Link} to="/board">게시판</Button>
+        <Button color="inherit" component={Link} to="/mypage">마이페이지</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
-export default observer(App);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="theme-yellow" style={{ height: '100vh', backgroundColor: 'var(--yellow2)' }}>
+          <NavBar />
+
+          <Grid container spacing={3} style={{ height: 'calc(100% - 64px)' }}>
+            <Grid item xs={3}> {/* 왼쪽 3칸 */}
+              <Box border={1} borderColor="grey.900" height="100%">
+                <Grid container direction="column" style={{ height: '100%' }}>
+                  <Grid item style={{ flex: 2 }}>
+                    <Box border={1} borderColor="grey.900" height="100%">
+                      <InfoSidebar />
+                    </Box>
+                  </Grid>
+                  <Grid item style={{ flex: 3 }}>
+                    <Box border={1} borderColor="grey.900" height="100%">
+                      <RankSystemSidebar />
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Grid>
+
+            <Grid item xs={9}> {/* 오른쪽 9칸 */}
+              <Box border={1} borderColor="grey.900" height="100%">
+                <Routes>
+                  <Route path="/" exact element={<MainPage />} />
+                  <Route path="/shelter" element={<ShelterPage />} />
+                  <Route path="/adoption" element={<AdoptionPage />} />
+                  <Route path="/board" element={<BoardPage />} />
+                  <Route path="/mypage" element={<MyPage />} />
+                </Routes>
+              </Box>
+            </Grid>
+          </Grid>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
